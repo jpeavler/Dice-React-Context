8cfa91c685dc595e02f6b10eb2e08ed05fb12293
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-28 15:35:20 -0600

    adjusted die component to leverage context

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,29 +1,20 @@
 import React, {Component} from 'react';
-import {RollContext} from './../context/roll-context'
+import {DiceBagContext} from '../context/roll-context'
 
-class Die extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            sides: this.props.sides,
-            result: 0
-        }
-        this.handleRoll = this.handleRoll.bind(this);
-    }
-    handleRoll(event){
-        event.preventDefault();
-        let rollResult = Math.ceil(Math.random() * this.state.sides);
-        this.setState({result: rollResult});
-    }
-    render(){
-        return(
-            <RollContext.Consumer>
-                <h3>Number of Sides: {this.state.sides}</h3>
-                {this.state.result}
-                <button onClick = {this.handleRoll}>Roll Die</button>
-            </RollContext.Consumer>
-        );
-    };
+function Die(props){
+    return(
+        <DiceBagContext.Consumer>
+            {({rollOne}) => (
+                <div>
+                    {die.numSides} : {die.currentRoll}
+                    <button
+                        onClick={() => rollOne(index)}
+                    >
+                        Roll
+                    </button>
+                </div>
+            )}
+        </DiceBagContext.Consumer>
+        )
 }
-
 export default Die;

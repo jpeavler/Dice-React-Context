d78137861e80023232ad9abfef3dd7a4d257a8bb
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-28 11:35:14 -0600

    Added previously coded components from older Dice Project

---
+++ src/components/Die.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class Die extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            sides: this.props.sides,
+            result: 0
+        }
+        this.handleRoll = this.handleRoll.bind(this);
+    }
+    handleRoll(event){
+        event.preventDefault();
+        let rollResult = Math.ceil(Math.random() * this.state.sides);
+        this.setState({result: rollResult});
+    }
+    render(){
+        return(
+            <>
+                <h3>Number of Sides: {this.state.sides}</h3>
+                {this.state.result}
+                <button onClick = {this.handleRoll}>Roll Die</button>
+            </>
+        );
+    };
+}
+
+export default Die;
